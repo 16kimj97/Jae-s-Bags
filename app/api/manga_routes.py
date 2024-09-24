6d2ea2cc513497ac211e3ba283a47b13474011fa
Justin <16kimj97@gmail.com>
2024-09-24 09:49:54 -0700

    manga get route

--- app/api/manga_routes.py
+++ app/api/manga_routes.py
@@ -2,3 +2,11 @@ from flask import Blueprint, jsonify, request
 from app.models import Manga, db
 from flask_login import current_user, login_required
 from datetime import datetime
+
+manga_routes = Blueprint('manga', __name__)
+
+@manga_routes.route('/')
+def manga_index():
+    mangas = Manga.query.all()
+
+    return [manga.to_dict() for manga in mangas]
